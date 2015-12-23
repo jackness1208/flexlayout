7e1658c7bacc00ed5b721cb5b2dd1d9f90a75e7e
jackness <jackness1208@qq.com>
2015-12-23 14:35:51 +0800

    [FIX]修复 IOS 手机内嵌页（weibo、微信） 页面初始化后 能左右滑动问题

--- js/flexlayout.js
+++ js/flexlayout.js
@@ -1,36 +1,55 @@
 !function(){
 
-	var docEl = document.documentElement,
+    var iUA = navigator.userAgent,
+        UA = {
+            YY: /YY[\/ ][0-9.]+/.test(iUA),
+            Android: /Android/i.test(iUA),
+            IOS: /(iPhone|iPod|iPad)/.test(iUA),
+            UCBrowser: /UCBrowser|UCWEB/.test(iUA)
+        };
+
+    var metaEl = document.createElement('meta'),
+        head = document.getElementsByTagName('head')[0],
+        docEl = document.documentElement,
         dpr = window.devicePixelRatio || 1,
         scWidth = window.screen.width,
-        scHeight = window.screen.height,
-        docW2H = docEl.clientWidth - docEl.clientHeight,
-        scW2H = scWidth - scHeight;
+        scHeight = window.screen.height;
 
+    metaEl.name = "viewport";
+    head.appendChild(metaEl);
 
-    // 临时方案 修复 ios yy 内嵌页 缩放比例不正常问题
-    if(/(iPhone|iPod|iPad).*YY\/[0-9.]+/.test(navigator.userAgent)){
-        dpr = 1;
+    if(UA.IOS) { // IOS
+        // 临时方案 修复 ios yy 内嵌页 缩放比例不正常问题
+        if(UA.YY){
+            dpr = 1;
+        }
+        scWidth = scWidth * dpr;
+        scHeight = scHeight * dpr;
+
+    } else { // Android
+        metaEl.content = "width=device-width, initial-scale=1, maximum-scale=1";
+
+        var docW2H = docEl.clientWidth - docEl.clientHeight,
+            scW2H = scWidth - scHeight;
+
+        // 修复部分机型 window.screen.width 始终代表屏幕较短的问题 即 横屏情况下 window.screen.width < window.screen.height
+        if(docW2H / Math.abs(docW2H) != (scW2H) / Math.abs(scW2H)){
+            scWidth = window.screen.height;
+            scHeight = window.screen.width;
+        }
+
+        // 修复部分机型 window.screen.width === docEl.clientWidth 情况, 正常应该是 window.screen.width == docEl.clientWidth * dpr
+        if(scWidth != docEl.clientWidth * dpr){
+            scWidth = Math.floor(scWidth * dpr);
+            scHeight = Math.floor(scHeight * dpr);
+        }
     }
-    
 
-    // 修复部分机型 window.screen.width 始终代表屏幕较短的问题 即 横屏情况下 window.screen.width < window.screen.height
-    if(docW2H / Math.abs(docW2H) != (scW2H) / Math.abs(scW2H)){
-        scWidth = window.screen.height;
-        scHeight = window.screen.width;
-    }
-
-    // 修复部分机型 window.screen.width === docEl.clientWidth 情况, 正常应该是 window.screen.width == docEl.clientWidth * dpr
-    var fixDpr = scWidth === docEl.clientWidth
-           ? dpr 
-           : 1
-           ,
-		screenWidth = Math.floor(scWidth * fixDpr),
-		screenHeight = Math.floor(scHeight * fixDpr),
-		clientWidth = screenWidth > screenHeight? screenHeight: screenWidth,
-		clientHeight = screenWidth > screenHeight? screenWidth: screenHeight,
+    var 
+		clientWidth = scWidth > scHeight? scHeight: scWidth,
+		clientHeight = scWidth > scHeight? screenWidth: scHeight,
 		fontEl = document.createElement('style'), 
-		metaEl = document.querySelector('meta[name="viewport"]'), 
+		
 		rem = clientWidth / 10, 
 		vrem = clientHeight / 10,
 		scale = 1 / dpr; // 设置viewport，进行缩放，达到高清效果 
@@ -38,10 +57,10 @@
     var cnt = [
         'width=' + clientWidth,
 		'initial-scale=' + scale,
-		'maximum-scale='+ scale
+        'maximum-scale='+ scale
     ];
 
-    if(!/UCBrowser|UCWEB/.test(navigator.userAgent)){
+    if(UA.Android && !UA.UCBrowser){
         // 修复部分低端机缩放后显示比例不正常问题 而 UC浏览器 碰到这个则会出现忽略 scale 直接 1:1 显示
         cnt.push('target-densitydpi=device-dpi');
     }
@@ -54,8 +73,9 @@
 
 	fontEl.innerHTML = [
 		'html{font-size:' + rem + 'px!important;}',
-		'@media screen and (min-width: '+ (clientHeight) +'px){',
+		'@media screen and (min-width: '+ clientHeight +'px){',
 			'html{font-size:'+ vrem +'px!important;}',
 		'}',
 	].join('');
 }();
+
